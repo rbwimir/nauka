96cf76a08d94f4596edfbd4d89cdd1ac95afb4aa
Robert Baran <rbwimir@me.com>
2014-01-30 13:23:13 +0100

    nsArray

--- nauka/ViewController.m
+++ nauka/ViewController.m
@@ -20,30 +20,62 @@
 {
     [super viewDidLoad];
     
-    NSString *a = @"slowo";
-    NSString *b = [NSString stringWithFormat:@"slowo2"];
-    NSString *c = [NSString stringWithFormat:@"%@----%@", b, a ];
-    
-    NSLog(@"%@",a);
-    NSLog(@"%@",b);
-    NSLog(@"%@",c);
-    
-    Osoba * osoba = [[Osoba alloc]init];
-    osoba.wiek = 12;
-    osoba.imie = @"Kamil";
-    NSLog(@"Osoba: %@", osoba);
-
-    Osoba *osoba2 = [[Osoba alloc]initWithName:@"Janek" andAge:13];
-    
-    NSLog(@"%@",osoba2);
-    
-    Student *student = [[Student alloc]init];
-    student.wiek = 10;
-    student.srednia = 26;
-    
+//    NSString *a = @"slowo";
+//    NSString *b = [NSString stringWithFormat:@"slowo2"];
+//    NSString *c = [NSString stringWithFormat:@"%@----%@", b, a ];
+//    
+//    NSLog(@"%@",a);
+//    NSLog(@"%@",b);
+//    NSLog(@"%@",c);
+//    
+//    Osoba * osoba = [[Osoba alloc]init];
+//    osoba.wiek = 12;
+//    osoba.imie = @"Kamil";
+//    NSLog(@"Osoba: %@", osoba);
+//
+//    Osoba *osoba2 = [[Osoba alloc]initWithName:@"Janek" andAge:13];
+//    
+//    NSLog(@"%@",osoba2);
+//    
+//    Student *student = [[Student alloc]init];
+//    student.wiek = 10;
+//    student.srednia = 26;
+//    
 //    osoba = student;
 //    student = osoba;
+    [super viewDidLoad];
     
+    NSMutableArray *mutArray = [[NSMutableArray alloc]init];
+    
+    [mutArray addObject:@"raz"];
+    
+    [mutArray addObject:@"dwa"];
+    
+    [mutArray addObject:@"trzy"];
+    
+    NSLog(@"%@",mutArray);
+    
+    for (int i =0; i<[mutArray count]; i++)
+    {
+        NSLog(@"%@", [mutArray objectAtIndex:i]);
+        id typ = [mutArray objectAtIndex:i];
+        NSString *string = (NSString *)typ;
+        NSLog(@"%@",string);
+    }
+    
+    for (NSString *s in mutArray)
+    {
+        NSLog(@"%@",s);
+    }
+
+    NSEnumerator *e = [mutArray objectEnumerator];
+    id object;
+    
+    while (object = [e nextObject])
+    {
+        
+        NSLog(@"%@",object);
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -61,6 +93,6 @@
 {
     [self.label setText:@"Zmieniony"];
     
-    Osoba *o;
+  
 }
 @end
